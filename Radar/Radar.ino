48e9c10e54e5f164adcf018ade802ae8354a6866
souvik0306 <souvikdatta123@gmail.com>
2021-08-28 00:24:19 +0530

    Radar Final Code

--- Radar/Radar.ino
+++ Radar/Radar.ino
@@ -1,15 +1,12 @@
 // Includes the Servo library
 #include <Servo.h>. 
-
 // Defines Tirg and Echo pins of the Ultrasonic Sensor
 const int trigPin = 10;
 const int echoPin = 11;
 // Variables for the duration and the distance
 long duration;
 int distance;
-
 Servo myServo; // Creates a servo object for controlling the servo motor
-
 void setup() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
@@ -20,26 +17,23 @@ void loop() {
   // rotates the servo motor from 15 to 165 degrees
   for(int i=15;i<=165;i++){  
   myServo.write(i);
-  delay(5);
+  delay(30);
   distance = calculateDistance();// Calls a function for calculating the distance measured by the Ultrasonic sensor for each degree
   
-  //Serial.print(i); // Sends the current degree into the Serial Port
+  Serial.print(i); // Sends the current degree into the Serial Port
   Serial.print(","); // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
   Serial.print(distance); // Sends the distance value into the Serial Port
   Serial.print("."); // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
-  Serial.print("\n");
   }
   // Repeats the previous lines from 165 to 15 degrees
   for(int i=165;i>15;i--){  
   myServo.write(i);
-  delay(5);
+  delay(30);
   distance = calculateDistance();
-  //Serial.print(i);
+  Serial.print(i);
   Serial.print(",");
   Serial.print(distance);
   Serial.print(".");
-  Serial.print("\n");
-  
   }
 }
 // Function for calculating the distance measured by the Ultrasonic sensor
