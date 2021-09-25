4457e389426155f859557a4966f3067cea1dff8a
souvik0306 <souvikdatta123@gmail.com>
2021-09-25 18:08:43 +0530

    Updated

--- barcode.py
+++ barcode.py
@@ -14,7 +14,7 @@ while True:
         pts = pts.reshape((1,-1,2))
         cv.polylines(frame,[pts],True,(0,255,0),3)
         pts2 = barcode.rect
-        cv.putText(frame,Data,(pts2[0],pts2[1]),cv.FONT_HERSHEY_PLAIN,0.9,(255,0,0),2)
+        cv.putText(frame,Data,(pts2[0],pts2[1]),cv.FONT_HERSHEY_COMPLEX_SMALL,0.9,(255,0,0),2)
     cv.imshow("Frame",frame)
     if cv.waitKey(1) & 0xFF == 27:
         break
