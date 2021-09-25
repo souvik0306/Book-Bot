05ef66eb821eb90083335b66d42698cc92a9aa3a
souvik0306 <souvikdatta123@gmail.com>
2021-09-25 18:00:29 +0530

    Updated Barcode

--- barcode.py
+++ barcode.py
@@ -9,10 +9,12 @@ while True:
 
     for barcode in decode(frame):
         print(barcode.data.decode('utf-8'))
+        Data = barcode.data.decode('utf-8')
         pts = np.array([barcode.polygon],np.int32)
         pts = pts.reshape((1,-1,2))
         cv.polylines(frame,[pts],True,(0,255,0),3)
-        
+        pts2 = barcode.rect
+        cv.putText(frame,Data,(pts2[0],pts2[1]),cv.FONT_HERSHEY_PLAIN,0.9,(255,0,0),2)
     cv.imshow("Frame",frame)
     if cv.waitKey(1) & 0xFF == 27:
         break
