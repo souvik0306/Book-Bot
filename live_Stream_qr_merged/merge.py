717d7ec30961c1bc7058f340943c57330ed552f5
GraniteMask <ratnadeepastronomy@gmail.com>
2021-10-08 14:38:35 +0530

    merged python file

--- live_Stream_qr_merged/merge.py
+++ live_Stream_qr_merged/merge.py
@@ -14,7 +14,25 @@ import time
 import threading
 import os
 
+def my_link():
+    cap = cv.VideoCapture(0)
 
+    while True:
+        _,frame = cap.read()
+    
+        for barcode in decode(frame):
+            print(barcode.data.decode('utf-8'))
+            Data = barcode.data.decode('utf-8')
+            pts = np.array([barcode.polygon],np.int32)
+            pts = pts.reshape((1,-1,2))
+            cv.polylines(frame,[pts],True,(0,255,0),3)
+            pts2 = barcode.rect
+            cv.putText(frame,Data,(pts2[0],pts2[1]),cv.FONT_HERSHEY_COMPLEX_SMALL,0.9,(255,0,255),2)
+        cv.imshow("Frame",frame)
+        if cv.waitKey(1) & 0xFF == 27:  # Press Escape Key to close all windows
+            break
+    cap.release()
+    cv.destroyAllWindows()
 
 pi_camera = VideoCamera(flip=False) # flip pi camera if upside down.
 
@@ -37,25 +55,7 @@ def video_feed():
     return Response(gen(pi_camera),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-def my_link():
-    cap = cv.VideoCapture(0)
 
-    while True:
-        _,frame = cap.read()
-    
-        for barcode in decode(frame):
-            print(barcode.data.decode('utf-8'))
-            Data = barcode.data.decode('utf-8')
-            pts = np.array([barcode.polygon],np.int32)
-            pts = pts.reshape((1,-1,2))
-            cv.polylines(frame,[pts],True,(0,255,0),3)
-            pts2 = barcode.rect
-            cv.putText(frame,Data,(pts2[0],pts2[1]),cv.FONT_HERSHEY_COMPLEX_SMALL,0.9,(255,0,255),2)
-        cv.imshow("Frame",frame)
-        if cv.waitKey(1) & 0xFF == 27:  # Press Escape Key to close all windows
-            break
-    cap.release()
-    cv.destroyAllWindows()
 
 
 if __name__ == '__main__':
